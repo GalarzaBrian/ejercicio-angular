baa9a3a1a398dd3f7c2a0e1270181729495a3892
BrianGalarza <briangalarza71@gmail.com>
2023-04-27 21:38:00 -0300

    creada importacion y exportacion de componentes

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -14,6 +14,11 @@ import { FooterComponent } from './footer/footer.component';
   ],
   imports: [
     CommonModule
+  ],
+  exports: [
+    HeaderComponent,    
+    NavComponent,
+    FooterComponent
   ]
 })
 export class SharedModule { }
